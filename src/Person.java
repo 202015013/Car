7a95f9f63e9d60a624d76cf17901a965b39a0a82
202015013 <xxspdlqjxx@naver.com>
2022-07-09 02:14:06 +0900

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,16 @@
+public class Person {
+    Car car;
+
+    void choiceCar(Car car){
+        this.car = car;
+        go();
+        open();
+    }
+    private void go(){
+        car.go();
+    }
+
+    private void open(){
+        car.open();
+    }
+}
